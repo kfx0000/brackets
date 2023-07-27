b6e52af780285ef8b2b3253f78235fd97964b07a
KFX <kfx0000@gmail.com>
2023-07-27 23:33:38 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  function removeInternal(arr) {
+    for(let count = 0; count <= arr.length; count++) {
+      for(let confPointer = 0; confPointer < bracketsConfig.length; confPointer++) {
+        if((arr[count] === bracketsConfig[confPointer][0])&&(arr[count+1] === bracketsConfig[confPointer][1])) {
+          arr.splice(count, 2);
+          removeInternal(arr);
+        }
+      }
+    }
+    if(arr.length === 0) return true; else return false;
+  }
+
+  return removeInternal(str.split(''));
 }
